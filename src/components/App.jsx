80a1976495b99e5d2c17272f382ac91aefec8f40
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-05-13 11:42:56 +0300

    add fetch function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import searchImages from "services";
+
 import Searchbar from "./Searchbar";
 import ImageGallery from "./ImageGallery";
 import Loader from './Loader'
@@ -8,10 +10,32 @@ import Modal from "./Modal";
 
 
 export class App extends Component  {
+state = {
+  query: ''
+}
+
+componentDidUpdate () {
+  searchImages(this.state.query, 1)
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+}
+  
+  handleSubmit = (e) => {
+    const query = e.target.elements.searchFormInput.value;
+    e.preventDefault();
+
+    this.setState({query});
+    
+    e.target.reset()
+
+  }
+
   render () {
+
+    
     return (
       <>
-      <Searchbar />
+      <Searchbar handleSubmit={this.handleSubmit}/>
       <ImageGallery />
       {/* <Loader /> */}
       <Button />
